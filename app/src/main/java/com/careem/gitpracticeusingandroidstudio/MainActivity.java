8bcffd34a04bc080fb5ea168e970dcfed235609c
Mehroze Yaqoob <mehrozeyaqub@gmail.com>
2018-01-10 12:09:51 +0500

    committing from master

--- app/src/main/java/com/careem/gitpracticeusingandroidstudio/MainActivity.java
+++ app/src/main/java/com/careem/gitpracticeusingandroidstudio/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         // Now that Master branch is up to dated. Its time to add some new stuff to development branch
 
         // Now its time to commit from master
+
+       // another commit
     }
 
 }
